1317fe88d2af7484a9fce1040da8931b42a5db3c
Yasar icli <yasaricli@gmail.com>
2020-01-06 18:05:28 +0300

    add login validate

--- handlers/loginHandler.go
+++ handlers/loginHandler.go
@@ -6,12 +6,15 @@ import (
 
 	"../utils"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/go-playground/validator.v10"
 )
 
 type LoginStruct struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=3"`
 }
+
+var validate *validator.Validate
 
 // Login User handler
 func LoginHandler(c *gin.Context) {
@@ -19,6 +22,21 @@ func LoginHandler(c *gin.Context) {
 
 	// parse body post data
 	c.BindJSON(&body)
+
+	// Validate LoginStruct form!
+	validate = validator.New()
+	validateError := validate.Struct(body)
+
+	if validateError != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error",
+			"data": gin.H{
+				"message": validateError.Error(),
+			},
+		})
+
+		return
+	}
 
 	user, err := utils.GetUser(body.Email)
 
